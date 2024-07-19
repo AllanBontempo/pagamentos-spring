4de9e2dbbe070000b7a793ae6913ca6953cd75c7
C1328047 Allan Bontempo <c1328047@interno.bb.com.br>
2024-07-19 00:00:13 -0300

    criação do projeto

---
+++ src/main/java/com/allanbontempo/pagamentos/PagamentosSpringApplication.java
@@ -0,0 +1,13 @@
+package com.allanbontempo.pagamentos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PagamentosSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PagamentosSpringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/allanbontempo/pagamentos/PagamentosSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.allanbontempo.pagamentos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PagamentosSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
